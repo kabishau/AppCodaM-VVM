2b6f9f44f29b663d369bf1797eaf09777f5ef411
user139405 <user139405@telehouse-moboware.dmarc.lga11.atlanticmetro.net>
2018-08-07 18:36:41 -0400

    update

---
+++ AppCodaMVVM/Controller/VC-Extension+TableViewDelegate.swift
@@ -0,0 +1,13 @@
+import Foundation
+
+import UIKit
+
+// MARK: - UITableView Delegate
+
+extension ViewController : UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+}
